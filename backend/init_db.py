b2a3246b3998ae13fcd26d2a237b16fcda02d1a8
Phatsakon_pj <phatsakon.son@dome.tu.ac.th>
2026-03-05 18:39:03 +0700

    Add new Database Design

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,10 +1,73 @@
 import sqlite3
 
 def init_db():
-    conn = sqlite3.connect("database.db")
+    conn = sqlite3.connect("parinya.db")
     cursor = conn.cursor()
 
-    # create tables here
+    # categories
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS categories (
+        category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+    )
+    """)
+
+    # users
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        role TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    # items
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS items (
+        item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_name TEXT NOT NULL,
+        price_per_kg REAL NOT NULL,
+        category_id INTEGER,
+        FOREIGN KEY (category_id) REFERENCES categories(category_id)
+    )
+    """)
+
+    # stock
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS stock (
+        stock_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_id INTEGER UNIQUE,
+        total_weight REAL NOT NULL,
+        last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (item_id) REFERENCES items(item_id)
+    )
+    """)
+
+    # transactions
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS transactions (
+        transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        transaction_type TEXT NOT NULL,
+        total_cost REAL NOT NULL,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+    )
+    """)
+
+    # transaction_items
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS transaction_items (
+        transaction_item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        transaction_id INTEGER,
+        item_id INTEGER,
+        weight REAL NOT NULL,
+        price_per_kg REAL NOT NULL,
+        FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id),
+        FOREIGN KEY (item_id) REFERENCES items(item_id)
+    )
+    """)
 
     conn.commit()
     conn.close()
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
+from init_db import init_db
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {'message': 'Welcome to the Copper System API!'}
+
+try:
+    init_db()
+    print("DB created")
+except Exception as e:
+    print(e)
